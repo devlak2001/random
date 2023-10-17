b54d2477fe75b1475fa1eb2fdc4a8ebb1c6128dd
Vladimir Karic <vladimirkaric2001@gmail.com>
2023-10-17 22:36:57 +0200

    initial commit, already created a functionality that creates circle slice for each entry

---
+++ app/spinner/page.tsx
@@ -0,0 +1,144 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+export default function Spinner() {
+  const canvasRef = useRef(null);
+  const [currentValue, setCurrentValue] = useState<string>("");
+  const [values, setValues] = useState<Array<string>>([]);
+  const [emptyInputError, setEmptyInputError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const colorsArray: Array<string> = [
+      "red",
+      "blue",
+      "yellow",
+      "orange",
+      "purple",
+      "green",
+    ];
+
+    const canvas: HTMLCanvasElement = canvasRef.current!;
+    const ctx = canvas.getContext("2d")!;
+
+    function drawSlices(
+      x: number,
+      y: number,
+      radius: number,
+      numOfSlices: number
+    ) {
+      const colors = colorsArray.slice(0, numOfSlices);
+      const sliceRadius = (Math.PI * 2) / numOfSlices;
+      console.log(colors);
+      if (numOfSlices) {
+        for (let i = 0; i < numOfSlices; i++) {
+          ctx.beginPath();
+          ctx.arc(x, y, radius, sliceRadius * i, sliceRadius * (i + 1));
+          ctx.lineTo(x, y);
+          ctx.fillStyle = colors[i];
+          ctx.fill();
+        }
+      } else {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, Math.PI * 2);
+        ctx.lineTo(x, y);
+        ctx.fillStyle = "red";
+        ctx.fill();
+      }
+      ctx.closePath();
+    }
+
+    drawSlices(canvas.width / 2, canvas.height / 2, 50, values.length);
+  }, [values]);
+
+  return (
+    <div className="flex items-start">
+      <canvas ref={canvasRef}></canvas>
+      <div className="pt-4">
+        <label className="relative flex items-center w-60">
+          <span className="sr-only">Search</span>
+          <span
+            className="absolute right-2 hover:bg-slate-200 hover:cursor-pointer transition-colors p-1"
+            onClick={(e) => {
+              if (currentValue !== "") {
+                setValues([...values, currentValue]);
+                setCurrentValue("");
+              } else {
+                setEmptyInputError(true);
+              }
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </span>
+          <span
+            className={`absolute left-3 bottom-10 text-xs text-red-600 transition-transform ${
+              emptyInputError ? "scale-100" : "scale-0"
+            }`}
+          >
+            You can't add an empty value
+          </span>
+
+          <input
+            onChange={(e) => {
+              setCurrentValue(e.target.value);
+              setEmptyInputError(false);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                if (currentValue !== "") {
+                  setValues([...values, currentValue]);
+                  setCurrentValue("");
+                } else {
+                  setEmptyInputError(true);
+                }
+              }
+            }}
+            className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+            placeholder="Add value to spinner"
+            type="text"
+            name="search"
+            value={currentValue}
+          />
+        </label>
+        <ul>
+          {values.map((el, index) => (
+            <li
+              key={index}
+              className="text-base w-60 flex items-center relative py-3 px-3 border-b-2"
+            >
+              <span>{el}</span>
+              <span className="absolute right-2 p-1 hover:bg-slate-200 hover:cursor-pointer transition-colors">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
